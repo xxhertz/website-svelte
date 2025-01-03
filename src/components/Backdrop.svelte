72d23050da9f03c1082223e191793752318f2dd9
hz <47288761+xxhertz@users.noreply.github.com>
2025-01-02 21:36:35 -0500

    fix: replace legacy slot api with sv5 render api
    svelte4 <slot /> has been replaced with svelte5 children props and @render

--- src/components/Backdrop.svelte
+++ src/components/Backdrop.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	let { children } = $props()
+</script>
+
 <div class="fixed top-0 left-0 h-full w-full cursor-[url('/cursor.png')_16_16,_auto] bg-[url('/background.png')] animate-[appear_1s_forwards_1]">
-	<slot />
+	{@render children?.()}
 </div>
